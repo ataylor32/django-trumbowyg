7706f315aea6d5ffa33c62ae811ef4aff11ab2a9
Adam Taylor <ataylor32@gmail.com>
2024-07-26 14:42:08 -0600

    Changed the "SEMANTIC" setting's default value to "true"

--- trumbowyg/settings.py
+++ trumbowyg/settings.py
@@ -3,4 +3,4 @@ from django.conf import settings
 UPLOAD_PATH = getattr(settings, "TRUMBOWYG_UPLOAD_PATH", "uploads/")
 THUMBNAIL_SIZE = getattr(settings, "TRUMBOWYG_THUMBNAIL_SIZE", None)
 TRANSLITERATE_FILENAME = getattr(settings, "TRUMBOWYG_TRANSLITERATE_FILENAME", False)
-SEMANTIC = getattr(settings, "TRUMBOWYG_SEMANTIC", "false")
+SEMANTIC = getattr(settings, "TRUMBOWYG_SEMANTIC", "true")
